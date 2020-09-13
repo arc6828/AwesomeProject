ebb70390e9f85481b506eed86d44085f6ef1b92e
arc6828 <chavalit.kow@gmail.com>
2020-09-13 17:20:46 +0700

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,12 @@
-import * as React from 'react';
+import React, {useContext} from 'react';
 import { View, Text, Button, TouchableOpacity } from 'react-native';
 
 import { Ionicons } from '@expo/vector-icons';
 import { fb } from '../db_config';
+import { AuthContext, AuthContextProvider } from "../hooks/AuthContext";
 
 export default function HomeScreen({ navigation }) {
+    const [user, setUser] = useContext(AuthContext);
 
     const onLogout = () => {
         fb.auth().signOut().then(function() {
@@ -22,6 +24,10 @@ export default function HomeScreen({ navigation }) {
     return (
         <View style={{ flex: 1 }}>
             <View style={{ flex : 1 , alignItems: 'center', justifyContent: 'center' }}>                
+                <View style={{ flexDirection : 'row' , padding : 30}}>
+                    <Text>Welcome, </Text>
+                    <Text style={{ color : "red"}}>{user.email}</Text>
+                </View>
                 <Ionicons name="md-home" size={50} color="#848484" />
                 <Text style={{ fontSize : 20 }}>Home Screen</Text>
                 <TouchableOpacity  onPress={() => navigation.navigate('BmiScreen') } >                    
